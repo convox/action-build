53a5ad6b9db4c3961771692aa942e88f38dd45f1
Robin Duncan Sharp <rob@sharp.id.au>
2022-03-24 16:18:25 +1100

    Allow decription to be passed in to the action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 echo "Building"
 export CONVOX_RACK=$INPUT_RACK
-SHORT_SHA=$(echo "${GITHUB_SHA}" | cut -c1-8)
-BRANCH=$(echo ${GITHUB_REF} | sed -e "s/refs\/heads\///g" | sed -e "s/\//-/g")
-DESCRIPTION="${GITHUB_ACTOR} ${BRANCH}#${SHORT_SHA} ${COMMIT_MESSAGE}"
-echo $DESCRIPTION
-release=$(convox build --description "${DESCRIPTION}" --app $INPUT_APP --id)
+release=$(convox build --app $INPUT_APP --description "$INPUT_DESCRIPTION" --id)
 if [ -z "$release" ]
 then
   echo "Build failed"
